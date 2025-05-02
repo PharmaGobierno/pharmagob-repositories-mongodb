dcd292b7e20f397d9dd53fef8fe2095e3d2dff76
Ariel Villeda <ariel.villeda@cargamos.com>
2025-05-02 12:53:29 -0600

    dispatch_at typo

--- pharmagob/mongodb_repositories/dispatch_records.py
+++ pharmagob/mongodb_repositories/dispatch_records.py
@@ -13,8 +13,8 @@ class DispatchRecordRepository(BaseMongoDbRepository):
         umu_id: Optional[str] = None,
         created_at_gt: Optional[int] = None,
         created_at_lt: Optional[int] = None,
-        dispatch_gt: Optional[int] = None,
-        dispatch_lt: Optional[int] = None,
+        dispatch_at_gt: Optional[int] = None,
+        dispatch_at_lt: Optional[int] = None,
         service: Optional[str] = None
     ) -> Tuple[int, List[dict]]:
         SEARCH_INDEX = "autocomplete_reference_id_range_created_at_and_dispatch"
@@ -34,13 +34,13 @@ class DispatchRecordRepository(BaseMongoDbRepository):
             if created_at_lt is not None:
                 created_at_range["lt"] = created_at_lt
             search["compound"]["must"].append({"range": created_at_range})
-        if dispatch_gt is not None or dispatch_lt is not None:
-            dispatch_range: Dict[str, Any] = {"path": "dispatch"}
-            if dispatch_gt is not None:
-                dispatch_range["gt"] = dispatch_gt
-            if dispatch_lt is not None:
-                dispatch_range["lt"] = dispatch_lt
-            search["compound"]["must"].append({"range": dispatch_range})
+        if dispatch_at_gt is not None or dispatch_at_lt is not None:
+            dispatch_at_range: Dict[str, Any] = {"path": "dispatch_at"}
+            if dispatch_at_gt is not None:
+                dispatch_at_range["gt"] = dispatch_at_gt
+            if dispatch_at_lt is not None:
+                dispatch_at_range["lt"] = dispatch_at_lt
+            search["compound"]["must"].append({"range": dispatch_at_range})
         search["compound"]["filter"] = []
         if umu_id:
             search["compound"]["filter"].append(
