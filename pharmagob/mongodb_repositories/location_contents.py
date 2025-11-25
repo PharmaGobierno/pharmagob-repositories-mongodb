d35aa8f0a395c8c34cb3ec29bef71862235ea59e
Ariel Villeda <ariel.villeda@cargamos.com>
2025-11-25 11:34:09 -0600

    new search function in location-contents

--- pharmagob/mongodb_repositories/location_contents.py
+++ pharmagob/mongodb_repositories/location_contents.py
@@ -65,3 +65,70 @@ class LocationContentRepository(BaseMongoDbRepository):
         aggregation_cursor = self._collection.aggregate(pipeline=pipeline)
         data: dict = aggregation_cursor.next()
         return data.get("count", 0), data.get("results", [])
+
+    def search_by_item_global(
+        self,
+        search_str: str,
+        *,
+        page: int = 1,
+        limit: int = BaseMongoDbRepository.DEFAULT_QUERY_LIMIT,
+        sort: Optional[Dict[str, int]] = None,
+        umu_id_in: Optional[List[str]] = None,
+        umu_id_not_in: Optional[List[str]] = None,
+        expiration_date_gt: Optional[int] = None,
+        expiration_date_lt: Optional[int] = None,
+        quantity_gt: Optional[int] = None,
+        quantity_lt: Optional[int] = None,
+        lot: Optional[str] = None
+    ) -> Tuple[int, List[dict]]:
+        SEARCH_INDEX = "autocomplete_item_id_range_expiration_date_range_quantity"
+        default_sort = sort
+        if default_sort is None:
+            default_sort = {"expiration_date": BaseMongoDbRepository.ASCENDING_ORDER}
+        search: dict = {
+            "index": SEARCH_INDEX,
+            "compound": {
+                "must": [{"autocomplete": {"query": search_str, "path": "item.id"}}]
+            },
+            "sort": default_sort,
+        }
+        if expiration_date_gt is not None or expiration_date_lt is not None:
+            expiration_date_range: Dict[str, Any] = {"path": "expiration_date"}
+            if expiration_date_gt is not None:
+                expiration_date_range["gt"] = expiration_date_gt
+            if expiration_date_lt is not None:
+                expiration_date_range["lt"] = expiration_date_lt
+            search["compound"]["must"].append({"range": expiration_date_range})
+        if quantity_gt is not None or quantity_lt is not None:
+            quantity_range: Dict[str, Any] = {"path": "quantity"}
+            if quantity_gt is not None:
+                quantity_range["gt"] = quantity_gt
+            if quantity_lt is not None:
+                quantity_range["lt"] = quantity_lt
+            search["compound"]["must"].append({"range": quantity_range})
+        search["compound"]["filter"] = []
+        if lot:
+            search["compound"]["filter"].append(
+                {"equals": {"path": "lot", "value": lot}}
+            )
+        if umu_id_in:
+            search["compound"]["filter"].append(
+                {"in": {"path": "umu_id", "value": umu_id_in}}
+            )
+        if umu_id_not_in:
+            search["compound"].setdefault("mustNot", []).extend(
+                [{"in": {"path": "umu_id", "value": umu_id_not_in}}]
+            )
+        pipeline: List[dict] = [
+            {"$search": search},
+            {
+                "$facet": {
+                    "results": [{"$skip": limit * (page - 1)}, {"$limit": limit}],
+                    "totalCount": [{"$count": "count"}],
+                }
+            },
+            {"$addFields": {"count": {"$arrayElemAt": ["$totalCount.count", 0]}}},
+        ]
+        aggregation_cursor = self._collection.aggregate(pipeline=pipeline)
+        data: dict = aggregation_cursor.next()
+        return data.get("count", 0), data.get("results", [])
