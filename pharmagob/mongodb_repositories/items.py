1284e1456e906cc0432914b3ed9d1f7c21afc29a
Ariel Villeda <ariel.villeda@cargamos.com>
2025-12-30 10:12:47 -0600

    method on items added

--- pharmagob/mongodb_repositories/items.py
+++ pharmagob/mongodb_repositories/items.py
@@ -1,5 +1,34 @@
+from typing import Iterator, List, Optional, Tuple, Union
+
 from .base import BaseMongoDbRepository
 
 
 class ItemsRepository(BaseMongoDbRepository):
-    pass
+    def get_by_foreign_id(
+        self,
+        foreign_id,
+        *,
+        umu_id: Optional[str] = None,
+        sort: Optional[List[Tuple[str, int]]] = None,
+        projection: Optional[Union[list, dict]] = None,
+        limit: Optional[int] = None,
+    ) -> Tuple[int, Iterator[dict]]:
+        """Retrieve a document based on its an paremeter.
+
+        Parameters:
+            foreign_id: The foreign_id of the documents.
+
+        Returns:
+            Tuple[int, Iterator[dict]]: The retrieved resources if found
+        """
+        filter = {"foreign_id": foreign_id}
+        if umu_id is not None:
+            filter["umu_id"] = umu_id
+        documents_count: int = self._collection.count_documents(filter)
+        documents_cursor = self._collection.find(
+            filter,
+            sort=sort,
+            projection=projection,
+            limit=(limit or self.DEFAULT_QUERY_LIMIT),
+        )
+        return documents_count, map(lambda item: item, documents_cursor)
